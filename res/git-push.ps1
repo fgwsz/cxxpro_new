ff688a5cc6a7d57e0ee49638883e0c0e5b8026b4
fgwsz <3191000458@qq.com>
2024-04-11 11:27:12 +0800

    增加一些空目录

--- res/git-push.ps1
+++ res/git-push.ps1
@@ -5,13 +5,18 @@ if($commit_info -eq "exit"){
 }else{
     git add include/*
     git add src/*
+    git add tests/*
     git add res/*
+    git add docs/*
+    git add license/*
     git add CMakeLists.txt
+    git add README.md
+    git add .gitignore
+    git add git-push.ps1
+    git add git-keep.ps1
     git add build*.ps1
     git add clean.ps1
-    git add README.md
-    git add git-push.ps1
-    git add .gitignore
+    git add run.ps1
     git commit -m $commit_info
     git push
 }
